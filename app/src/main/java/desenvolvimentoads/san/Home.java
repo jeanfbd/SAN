4d40737800c180ff86ccb9a2cf536979998efc1b
jeanfbd <jeanfbd@hotmail.com>
2017-07-10 13:18:00 -0300

    Criacao branch RF1-Tela Inicial

--- app/src/main/java/desenvolvimentoads/san/Home.java
+++ app/src/main/java/desenvolvimentoads/san/Home.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Home extends AppCompatActivity {
-    //teste de commit
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
