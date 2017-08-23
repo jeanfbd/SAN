dd86e23c6756e05ae3e349c8c6961cb08027d527
jeanfbd <jeanfbd@hotmail.com>
2017-08-23 11:42:35 -0300

    Criado login pelo Google Sign In

--- app/src/main/java/desenvolvimentoads/san/MenuInicial.java
+++ app/src/main/java/desenvolvimentoads/san/MenuInicial.java
@@ -1,6 +1,8 @@
 package desenvolvimentoads.san;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -12,13 +14,33 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MenuInicial extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.OnConnectionFailedListener  {
+
+    private GoogleApiClient googleApiClient;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    private FirebaseAuth firebaseAuth;
+
+    private String userName;
+    private String userEmail;
+    private String userId;
+
+    private static final String TAG = "MenuInicial";
 
     private FragmentManager fragmentManager;
     static FloatingActionButton fab;
@@ -29,6 +51,30 @@ public class MenuInicial extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_inicial);
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    setUserData(user);
+                } else {
+                    goLogInScreen();
+                }
+            }
+        };
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -206,6 +252,70 @@ public class MenuInicial extends AppCompatActivity
         fragmentTransaction.commit();
     }
 
+    private void setUserData(FirebaseUser user) {
+        userName = user.getDisplayName();
+        userEmail = user.getEmail();
+        userId = user.getUid();
+
+        Log.i(TAG, "setUserData: Nome "+userName+", Email: "+userEmail+", ID: "+userId);
+//        Glide.with(this).load(user.getPhotoUrl()).into(photoImageView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);
+    }
+
+    private void goLogInScreen() {
+        Intent intent = new Intent(this, TelaInicial.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void logOut(View view) {
+        firebaseAuth.signOut();
+
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if (status.isSuccess()) {
+                    goLogInScreen();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Não foi possível encerrar a sessão", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void revoke(View view) {
+        firebaseAuth.signOut();
+
+        Auth.GoogleSignInApi.revokeAccess(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if (status.isSuccess()) {
+                    goLogInScreen();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Não foi possível revokar a sessão", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (firebaseAuthListener != null) {
+            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+        }
+    }
 
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
--- app/src/main/java/desenvolvimentoads/san/TelaInicial.java
+++ app/src/main/java/desenvolvimentoads/san/TelaInicial.java
@@ -2,19 +2,52 @@ package desenvolvimentoads.san;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import desenvolvimentoads.san.DAO.ConfigFireBase;
 
 /**
  * Created by jeanf on 10/07/2017.
  */
 
-public class TelaInicial extends Activity {
+public class TelaInicial extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
-    protected static final int TIME_RUNTIME = 10000;
+    private GoogleApiClient googleApiClient;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    private FirebaseAuth firebaseAuth;
 
-    protected boolean pbActive;
+    private SignInButton signInButton;
+
+    private static final String TAG = "TelaInicial";
+
+    private static final int SIGN_IN_CODE = 9001;
+    private static final int TIME_RUNTIME = 5000;
+    private boolean pbActive;
 
     protected ProgressBar progressBar;
 
@@ -23,17 +56,128 @@ public class TelaInicial extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_inicial);
 
-        /*
-        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
-        Thread timerThread = new Thread(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        signInButton = (SignInButton) findViewById(R.id.signInButton);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setColorScheme(SignInButton.COLOR_DARK);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run(){
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, SIGN_IN_CODE);
+            }
+        });
+
+
+        firebaseAuth = ConfigFireBase.getAuth();
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    goMainScreen();
+                }else{
+                    Toast.makeText(TelaInicial.this, "Usuário NULL", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(TelaInicial.this, "Falha na conexão", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SIGN_IN_CODE) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            firebaseAuthWithGoogle(result.getSignInAccount());
+        } else {
+            Toast.makeText(this, "Não foi possível iniciar a sessão", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount signInAccount) {
+        progressBar.setVisibility(View.VISIBLE);
+        signInButton.setVisibility(View.GONE);
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                progressBar.setVisibility(View.GONE);
+                signInButton.setVisibility(View.VISIBLE);
+
+                if (!task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Não foi possível autenticar no Firebase", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void goMainScreen() {
+        Intent intent = new Intent(this, MenuInicial.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (firebaseAuthListener != null) {
+            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+        }
+    }
+
+    public void updateProgressBar(final int timePassed) {
+        if (null != progressBar) {
+            final int progress = progressBar.getMax() * timePassed / TIME_RUNTIME;
+            progressBar.setProgress(progress);
+        }
+    }
+
+//    public void onContinue() {
+//        Log.d("mensagemFinal", "Sua barra concluiu");
+//    }
+
+    public void threadTelaInicial(){
+        Thread timerThread = new Thread() {
+            @Override
+            public void run() {
                 try {
                     sleep(TIME_RUNTIME);
-                }catch (InterruptedException e){
+                } catch (InterruptedException e) {
                     e.printStackTrace();
-                }finally {
+                } finally {
                     Intent intent = new Intent(TelaInicial.this, MenuInicial.class);
                     startActivity(intent);
                 }
@@ -41,51 +185,28 @@ public class TelaInicial extends Activity {
             }
         };
 
-        final Thread timerBar = new Thread(){
+        final Thread timerBar = new Thread() {
             @Override
             public void run() {
                 pbActive = true;
                 try {
                     int waited = 0;
-                    while (pbActive && (waited < TIME_RUNTIME)){
+                    while (pbActive && (waited < TIME_RUNTIME)) {
                         sleep(200);
-                        if (pbActive){
-                            waited+=200;
+                        if (pbActive) {
+                            waited += 200;
                             updateProgressBar(waited);
                         }
                     }
-                }catch (InterruptedException e){
+                } catch (InterruptedException e) {
                     e.printStackTrace();
-                }finally {
-                    onContinue();
+                } finally {
+//                    onContinue();
                 }
             }
         };
 
         timerThread.start();
         timerBar.start();
-
-        */
-
-        Intent intent = new Intent(TelaInicial.this, MenuInicial.class);
-        startActivity(intent);
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        finish();
-    }
-
-    public void updateProgressBar(final int timePassed){
-        if (null != progressBar){
-            final int progress = progressBar.getMax() * timePassed /TIME_RUNTIME;
-            progressBar.setProgress(progress);
-        }
-    }
-
-    public void onContinue(){
-        Log.d("mensagemFinal", "Sua barra concluiu");
     }
 }
